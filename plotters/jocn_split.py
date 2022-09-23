dbc35552537bbd736e6efcf5755f10effe70479a
Georgios Drainakis <giorgos.drainakis@iccs.gr>
2022-09-23 18:23:16 +0300

    added jocn files

--- plotters/jocn_split.py
+++ plotters/jocn_split.py
@@ -293,39 +293,39 @@ def plot_thru_e2e(data,strategy):
 
     x_label='Total traffic load (Tbps)'
     y_label='Bitrate (Tbps)'
-    legend_loc='lower right'
+    legend_loc='center right'
 
     # Clean my loads and assign max thru values and limits
     if data==1600:
-        max_thru = 16*400+16*100
-        x_lim_begin = -1
-        x_lim_end=10000
+        max_thru = 16*0.4+16*0.1
+        x_lim_begin = -0.1
+        x_lim_end=10
         if strategy=='go':
             load, thru, drop = clean_load_thru_drop(waa_1600_go_e2e_load_total_bps_avg, waa_1600_go_e2e_thru_total_bps_avg,
-                                               waa_1600_go_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_1600_go_e2e_drop_total_bps_avg,cleaning_factor=1e12)
         elif strategy=='stay':
             load, thru, drop = clean_load_thru_drop(waa_1600_stay_e2e_load_total_bps_avg, waa_1600_stay_e2e_thru_total_bps_avg,
-                                               waa_1600_stay_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_1600_stay_e2e_drop_total_bps_avg,cleaning_factor=1e12)
     elif data==2400:
-        max_thru = 16*400+16*100
-        #    x_lim_begin = 1.95
-        #    x_lim_end=10
+        max_thru = 16*0.4+16*0.1
+        x_lim_begin = -0.1
+        x_lim_end=10
         if strategy=='go':
             load, thru, drop = clean_load_thru_drop(waa_2400_go_e2e_load_total_bps_avg, waa_2400_go_e2e_thru_total_bps_avg,
-                                               waa_2400_go_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_2400_go_e2e_drop_total_bps_avg,cleaning_factor=1e12)
         elif strategy=='stay':
             load, thru, drop = clean_load_thru_drop(waa_2400_stay_e2e_load_total_bps_avg, waa_2400_stay_e2e_thru_total_bps_avg,
-                                               waa_2400_stay_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_2400_stay_e2e_drop_total_bps_avg,cleaning_factor=1e12)
     elif data==3200:
-        max_thru = 16*400+16*100
-        #    x_lim_begin = 1.95
-        #    x_lim_end=10
+        max_thru = 16*0.4+16*0.1
+        x_lim_begin = -0.1
+        x_lim_end=10
         if strategy=='go':
             load, thru, drop = clean_load_thru_drop(waa_3200_go_e2e_load_total_bps_avg, waa_3200_go_e2e_thru_total_bps_avg,
-                                               waa_3200_go_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_3200_go_e2e_drop_total_bps_avg,cleaning_factor=1e12)
         elif strategy=='stay':
             load, thru, drop = clean_load_thru_drop(waa_3200_stay_e2e_load_total_bps_avg, waa_3200_stay_e2e_thru_total_bps_avg,
-                                               waa_3200_stay_e2e_drop_total_bps_avg,cleaning_factor=1e9)
+                                               waa_3200_stay_e2e_drop_total_bps_avg,cleaning_factor=1e12)
     nominal_thru=[]
     for el in load:
         nominal_thru.append(max_thru)
@@ -559,6 +559,146 @@ def plot_qdelays_e2e(data, strategy,toplot):
     ax1.tick_params(axis='both', which='minor', labelsize=_TICK_PARAMS)
 
     plt.show()
+def plot_delays_n_qdelays_e2e(data, strategy,toplot):
+    plt.rcParams["font.weight"] = "bold"
+    plt.rcParams["axes.labelweight"] = "bold"
+
+    x_label='Total traffic load (Tbps)'
+    y_label='End-to-end delay (ms)'
+    legend_loc='center right'
+    y_lim_begin=0
+    y_lim_end=1e-1
+    # Clean my loads and assign limits
+    if data==1600:
+        x_lim_begin = 1.95
+        x_lim_end=10
+        if strategy=='go':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_1600_go_e2e_load_total_bps_avg,
+                                                                                  waa_1600_go_e2e_delay_total_avg,
+                                                                                  waa_1600_go_e2e_delay_high_avg,
+                                                                                  waa_1600_go_e2e_delay_med_avg,
+                                                                                  waa_1600_go_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_1600_go_e2e_load_total_bps_avg,
+                                                                                  waa_1600_go_e2e_qdelay_total_avg,
+                                                                                  waa_1600_go_e2e_qdelay_high_avg,
+                                                                                  waa_1600_go_e2e_qdelay_med_avg,
+                                                                                  waa_1600_go_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+        elif strategy=='stay':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_1600_stay_e2e_load_total_bps_avg,
+                                                                                  waa_1600_stay_e2e_delay_total_avg,
+                                                                                  waa_1600_stay_e2e_delay_high_avg,
+                                                                                  waa_1600_stay_e2e_delay_med_avg,
+                                                                                  waa_1600_stay_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_1600_stay_e2e_load_total_bps_avg,
+                                                                                  waa_1600_stay_e2e_qdelay_total_avg,
+                                                                                  waa_1600_stay_e2e_qdelay_high_avg,
+                                                                                  waa_1600_stay_e2e_qdelay_med_avg,
+                                                                                  waa_1600_stay_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+    elif data==2400:
+        #    x_lim_begin = 1.95
+        #    x_lim_end=10
+        if strategy=='go':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_2400_go_e2e_load_total_bps_avg,
+                                                                                  waa_2400_go_e2e_delay_total_avg,
+                                                                                  waa_2400_go_e2e_delay_high_avg,
+                                                                                  waa_2400_go_e2e_delay_med_avg,
+                                                                                  waa_2400_go_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_2400_go_e2e_load_total_bps_avg,
+                                                                                  waa_2400_go_e2e_qdelay_total_avg,
+                                                                                  waa_2400_go_e2e_qdelay_high_avg,
+                                                                                  waa_2400_go_e2e_qdelay_med_avg,
+                                                                                  waa_2400_go_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+        elif strategy=='stay':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_2400_stay_e2e_load_total_bps_avg,
+                                                                                  waa_2400_stay_e2e_delay_total_avg,
+                                                                                  waa_2400_stay_e2e_delay_high_avg,
+                                                                                  waa_2400_stay_e2e_delay_med_avg,
+                                                                                  waa_2400_stay_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_2400_stay_e2e_load_total_bps_avg,
+                                                                                  waa_2400_stay_e2e_qdelay_total_avg,
+                                                                                  waa_2400_stay_e2e_qdelay_high_avg,
+                                                                                  waa_2400_stay_e2e_qdelay_med_avg,
+                                                                                  waa_2400_stay_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+    elif data==3200:
+        #    x_lim_begin = 1.95
+        #    x_lim_end=10
+        if strategy=='go':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_3200_go_e2e_load_total_bps_avg,
+                                                                                  waa_3200_go_e2e_delay_total_avg,
+                                                                                  waa_3200_go_e2e_delay_high_avg,
+                                                                                  waa_3200_go_e2e_delay_med_avg,
+                                                                                  waa_3200_go_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_3200_go_e2e_load_total_bps_avg,
+                                                                                  waa_3200_go_e2e_qdelay_total_avg,
+                                                                                  waa_3200_go_e2e_qdelay_high_avg,
+                                                                                  waa_3200_go_e2e_qdelay_med_avg,
+                                                                                  waa_3200_go_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+        elif strategy=='stay':
+            load_delay, avg_delay, high_delay, med_delay, low_delay = clean_load_delays(waa_3200_stay_e2e_load_total_bps_avg,
+                                                                                  waa_3200_stay_e2e_delay_total_avg,
+                                                                                  waa_3200_stay_e2e_delay_high_avg,
+                                                                                  waa_3200_stay_e2e_delay_med_avg,
+                                                                                  waa_3200_stay_e2e_delay_low_avg,
+                                                                                  cleaning_factor=1e12)
+            load_q, avg_q, high_q, med_q, low_q = clean_load_delays(waa_3200_stay_e2e_load_total_bps_avg,
+                                                                                  waa_3200_stay_e2e_qdelay_total_avg,
+                                                                                  waa_3200_stay_e2e_qdelay_high_avg,
+                                                                                  waa_3200_stay_e2e_qdelay_med_avg,
+                                                                                  waa_3200_stay_e2e_qdelay_low_avg,
+                                                                                  cleaning_factor=1e12)
+
+    fig, ax1 = plt.subplots(constrained_layout=True)
+    #ax1.set_yscale('symlog')
+    #ax1.plot(load, avg_delay,'k-.', label="Avg",linewidth=4)
+    limit=0
+    _LINEWIDTH=7
+    _LABEL_SIZE=45
+    _LEGEND_SIZE=45
+    _TICK_PARAMS=45
+
+    if toplot in ['only_avg','all']:
+        ax1.semilogy(load_delay[limit:], avg_delay[limit:], 'b', label="total delay", linewidth=_LINEWIDTH+1)
+        ax1.semilogy(load_q[limit:], avg_q[limit:], 'r', label="queuing delay", linewidth=_LINEWIDTH)
+    if toplot in ['only_hml','all']:
+        ax1.semilogy(load_delay[limit:], high_delay[limit:],'r', label="High",linewidth=_LINEWIDTH)
+        ax1.semilogy(load_delay[limit:], med_delay[limit:],'g', label="Med",linewidth=_LINEWIDTH)
+        ax1.semilogy(load_delay[limit:], low_delay[limit:],'b', label="Low",linewidth=_LINEWIDTH)
+
+    try:
+        ax1.set_xlabel(x_label, fontsize=_LABEL_SIZE)
+    except:
+        pass
+    try:
+        ax1.set_ylabel(y_label, fontsize=_LABEL_SIZE)
+    except:
+        pass
+    try:
+        ax1.set_xlim(x_lim_begin,x_lim_end)
+    except:
+        pass
+    try:
+        ax1.set_ylim(y_lim_begin,y_lim_end)
+    except:
+        pass
+    try:
+        ax1.legend(loc=legend_loc, fontsize=_LEGEND_SIZE)
+    except:
+        ax1.legend(loc='upper left', fontsize=_LEGEND_SIZE)
+    ax1.grid(True, which='major', axis='both')
+    ax1.tick_params(axis='both', which='major', labelsize=_TICK_PARAMS)
+    ax1.tick_params(axis='both', which='minor', labelsize=_TICK_PARAMS)
+
+    plt.show()
 def plot_compare_delay_bar():
     plt.rcParams["font.weight"] = "bold"
     plt.rcParams["axes.labelweight"] = "bold"
@@ -633,23 +773,106 @@ def plot_compare_delay_bar():
     ax1.tick_params(axis='both', which='minor', labelsize=_TICK_PARAMS)
 
     plt.show()
+def plot_compare_delay_bar_B():
+    plt.rcParams["font.weight"] = "bold"
+    plt.rcParams["axes.labelweight"] = "bold"
 
+    x_label='Total traffic load (Tbps)'
+
+    x_lim_begin=-2
+    x_lim_end=12
+    y_label='End-to-end delay (ms)'
+    legend_loc='lower left'
+
+    fig, ax1 = plt.subplots(constrained_layout=False)
+    ax1.set_yscale('log')
+
+    load=[2,4,6,8,10]
+    high_go=[0.000476,0.000506,0.000777,0.000989,0.00103]
+    med_go = [0.000685,0.000748,0.0066,0.0272,0.0322]
+    low_go = [0.0211,0.0222,0.1,0.225,0.237]
+    high_stay=[0.000253,0.000253,0.000321,0.000437,0.000464]
+    med_stay = [0.00079,0.00079,0.00814,0.0318,0.0382]
+    low_stay = [0.0213,0.024,0.125,0.266,0.277]
+
+    for i in range(0,len(load)):
+        hh=(100*(high_go[i]-high_stay[i]))/high_go[i]
+        mm = (100 * (med_go[i] - med_stay[i])) / med_go[i]
+        ll = (100 * (low_go[i] - low_stay[i])) / low_go[i]
+        print('Load='+str(load[i]))
+        print('high change='+str(hh))
+        print('med change=' + str(mm))
+        print('low change=' + str(ll))
+        print('----')
+
+    mini_size = 0.14
+    big_size = 0.55
+    width = 0.2
+
+    x_med = load
+    x_med_2400 = [x - mini_size for x in x_med]
+    x_med_3200 = [x + mini_size for x in x_med]
+
+    x_high = [x - big_size for x in x_med]
+    x_high_2400 = [x - mini_size for x in x_high]
+    x_high_3200  = [x + mini_size for x in x_high]
+
+    x_low = [x + big_size for x in x_med]
+    x_low_2400 = [x - mini_size for x in x_low]
+    x_low_3200  = [x + mini_size for x in x_low]
+
+
+    ax1.bar(x_high_2400, high_go, color='white',edgecolor='red', label="None/High",linewidth=4, width=width,hatch='//')
+    ax1.bar(x_med_2400, med_go,  color='white',edgecolor='green', label="None/Med",linewidth=4, width=width,hatch='//')
+    ax1.bar(x_low_2400, low_go,  color='white',edgecolor='blue', label="None/Low",linewidth=4, width=width,hatch='//')
+    ax1.bar(x_high_3200, high_stay, color='white',edgecolor='red',  label="StayIn/Low",linewidth=4, width=width, hatch='o')
+    ax1.bar(x_med_3200, med_stay, color='white',edgecolor='green', label="StayIn/Med",linewidth=4, width=width, hatch='o')
+    ax1.bar(x_low_3200, low_stay, color='white',edgecolor='blue', label="StayIn/High",linewidth=4, width=width, hatch='o')
+    ax1.set_xticks([2,4,6,8,10])
+
+    _LINEWIDTH=7
+    _LABEL_SIZE=45
+    _LEGEND_SIZE=33
+    _TICK_PARAMS=45
+    try:
+        ax1.set_xlabel(x_label, fontsize=_LABEL_SIZE)
+    except:
+        pass
+    try:
+        ax1.set_ylabel(y_label, fontsize=_LABEL_SIZE)
+    except:
+        pass
+    try:
+        ax1.set_xlim(x_lim_begin,x_lim_end)
+    except:
+        pass
+    try:
+        ax1.legend(loc='upper left', fontsize=_LEGEND_SIZE)
+    except:
+        ax1.legend(loc='upper left', fontsize=_LEGEND_SIZE)
+    ax1.grid(True, which='major', axis='both')
+    ax1.tick_params(axis='both', which='major', labelsize=_TICK_PARAMS)
+    ax1.tick_params(axis='both', which='minor', labelsize=_TICK_PARAMS)
+
+    plt.show()
 # Group A: Plots for basic scenario (1600-go)
 #plot_thru_intra(data=1600,strategy='go') # data in [1600,2400,3200], strategy in ['go','stay']
 #plot_thru_inter(data=1600,strategy='go')
 #plot_thru_e2e(data=1600, strategy='go')
 #plot_delays_e2e(data=1600, strategy='go',toplot='only_avg')
 #plot_qdelays_e2e(data=1600, strategy='go',toplot='only_avg')
+#plot_delays_n_qdelays_e2e(data=1600, strategy='go',toplot='only_avg')
 
 # Group B: PLot for strategy (1600-go and stay variations)
 #plot_delays_e2e(data=1600, strategy='go',toplot='all')
 #plot_delays_e2e(data=1600, strategy='stay',toplot='all')
+plot_compare_delay_bar_B()
 
 # Group C: PLot for scaling number of servers (1600,2400,3200 stay)
 #for data in [1600,2400,3200]:
 #    plot_thru_per_server_intra(data=data,strategy='stay') # data in [1600,2400,3200], strategy in ['go','stay']
 
-for data in [1600,2400,3200]:
-    plot_delays_e2e(data=data, strategy='stay',toplot='only_hml') # toplot in ['only_avg', 'only_hml', 'all']
+#for data in [1600,2400,3200]:
+#    plot_delays_e2e(data=data, strategy='stay',toplot='only_hml') # toplot in ['only_avg', 'only_hml', 'all']
 
 #plot_compare_delay_bar()
